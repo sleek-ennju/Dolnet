ce6e418c91b21a4bc773a0f1635a1283d5a82593
Hizaram <promisevector@gmail.com>
2023-05-29 23:15:38 +0100

    [Feat]: Add the user class module

---
+++ dolianet.com/backend/models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""The user model module"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+
+Base = declarative_base()
+
+
+class User(Base):
+    """The User class representation"""
+    __tablename__ = "users"
+    user_id = Column(Integer, primary_key=True, nullable=False)
+    user_name = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False)
+    user_phrase = Column(String(255), nullable=False)
+    password = Column(String(255), nullable=False)
